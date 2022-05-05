45023c2706008d28574383a87eea2601f3cf89cf
Lían König <lk.liankonig@gmail.com>
2022-05-05 18:32:43 -0300

    mapping the entities with jpa

--- src/main/java/com/dio/santander/banckline/api/model/Conta.java
+++ src/main/java/com/dio/santander/banckline/api/model/Conta.java
@@ -1,7 +1,15 @@
 package com.dio.santander.banckline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+
+    @Column(name = "conta_numero")
     private Long numero;
+
+    @Column(name = "conta_saldo")
     private Double saldo;
 
     public Long getNumero() {
--- src/main/java/com/dio/santander/banckline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/banckline/api/model/Correntista.java
@@ -1,9 +1,29 @@
 package com.dio.santander.banckline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "cpf", length = 20)
     private String cpf;
+
+    @Column(name = "nome", length = 60)
     private String nome;
+
+    @Embedded
+    @Column(name = "conta")
     private Conta conta;
 
     public Integer getId() {
