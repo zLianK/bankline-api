fca6b576f86921b91f7a235875868ff8881725ab
Lían König <lk.liankonig@gmail.com>
2022-05-07 15:55:03 -0300

    adding list movimentacao by id function

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -9,6 +9,7 @@ import com.dio.santander.bankline.api.service.MovimentacaoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,11 @@ public class MovimentacaoController {
     @GetMapping
     public List<Movimentacao> findAll() {
         return movimentacaoRepository.findAll();
+    }
+
+    @GetMapping("/{idConta}")
+    public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta) {
+        return movimentacaoRepository.findByIdConta(idConta);
     }
 
     @PostMapping
